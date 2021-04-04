a3588951ba3f6d96fbcd823fddce75d0db3091ad
mgravemeyer <m.gravemeyer@icloud.com>
2021-04-04 11:36:47 +0200

    Initial Commit

---
+++ pdf-editor/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  pdf-editor
+//
+//  Created by Maximilian Gravemeyer on 04.04.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ pdf-editor/pdf_editorApp.swift
@@ -0,0 +1,17 @@
+//
+//  pdf_editorApp.swift
+//  pdf-editor
+//
+//  Created by Maximilian Gravemeyer on 04.04.21.
+//
+
+import SwiftUI
+
+@main
+struct pdf_editorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
