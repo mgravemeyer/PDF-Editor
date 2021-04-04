9ec842ded519ac123e6211c384c6f6d4b0623884
mgravemeyer <m.gravemeyer@icloud.com>
2021-04-04 11:45:55 +0200

    added PDFListView

---
+++ pdf-editor/PDFListView.swift
@@ -0,0 +1,35 @@
+import SwiftUI
+
+struct PDFListView: View {
+    
+    init() {
+        let url = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first
+        do {
+            let urls = try FileManager.default.contentsOfDirectory(at: url!, includingPropertiesForKeys: nil)
+            for url in urls {
+                print(url.lastPathComponent)
+                pdfs.append(url.lastPathComponent)
+            }
+        } catch {
+            print(error.localizedDescription)
+        }
+    }
+    
+    var pdfs = [String]()
+    
+    var body: some View {
+        NavigationView {
+            List {
+                ForEach(pdfs, id: \.self) {
+                    NavigationLink($0, destination: ContentView())
+                }
+            }
+        }
+    }
+}
+
+struct PDFListView_Previews: PreviewProvider {
+    static var previews: some View {
+        PDFListView()
+    }
+}
